623f328e35cc98e585d83d41c2d2b22bdcc1ba53
Isabel Parra <isabelparra@Isabels-MBP.T-mobile.com>
2018-10-15 15:45:11 -0400

    heroku

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/yogaJournal");
+// mongoose.connect("mongodb://localhost/yogaJournal");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/yogaJournal";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // var db = mongoose.connection;
 // db.on("error", function(error) {
